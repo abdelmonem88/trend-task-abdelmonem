2f771c35945a6d27de16ed3ffdf39e667d439ba7
abdelmonem88 <abdelmonem1888@gmail.com>
2025-06-08 04:44:24 +0300

    adjust linting setup and remove unused var

--- components/admin-mode.tsx
+++ components/admin-mode.tsx
@@ -6,7 +6,6 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Switch } from "@/components/ui/switch";
 import { Checkbox } from "@/components/ui/checkbox";
-import { Palette } from "lucide-react";
 import { SVG } from "@/components/shared/SVG";
 
 interface AdminModeProps {
